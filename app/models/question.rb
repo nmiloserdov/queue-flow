fce565a373d89756323b36dccc2a04bac994d5b5
Miloserdov Nikita <nmiloserdov09@gmail.com>
2016-03-13 19:24:42 +0300

    Add Question's & answer & test

---
+++ app/models/question.rb
@@ -0,0 +1,6 @@
+class Question < ActiveRecord::Base
+  has_many :answers
+  
+  validates :title, presence: true
+  validates :body, presence: true
+end
---
+++ spec/models/question_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+RSpec.describe Question, type: :model do
+
+  it { should have_many :answers }
+  
+  it { should validate_presence_of :title }
+  it { should validate_presence_of :body }
+                         
+  describe "attributes" do
+
+    question = Question.new(title: "Am i new rspec?", body: "Help me, i don't now")
+    
+    it "have right :title" do
+      expect(question).to have_attributes(title: "Am i new rspec?")
+    end  
+    
+    it "have right :body" do
+      expect(question).to have_attributes(body: "Help me, i don't now")
+    end
+    
+    it "set new :title" do
+      question.title = "test"
+      expect(question.title).to eq("test")
+    end
+    
+    it "set new :body" do
+      question.body = "test"
+      expect(question.title).to eq("test")
+    end
+  end
+  
+end
