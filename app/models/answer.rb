bc4a471f501816989e32ca44d63b6ef0d95cf5e7
Miloserdov Nikita <nmiloserdov09@gmail.com>
2016-03-14 11:31:49 +0300

    Add rubocop

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,6 +1,5 @@
 class Answer < ActiveRecord::Base
   belongs_to :question
-  validates :question_id, presence:true
-  
-  validates :body, presence: true
+
+  validates :body, :question_id, presence: true
 end
