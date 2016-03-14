f8fe7b951cf60c9baf7243522f8a98b3f91927d0
Miloserdov Nikita <nmiloserdov09@gmail.com>
2016-03-14 16:35:41 +0300

    Change spaces

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe Answer, type: :model do
   it { should have_db_index :question_id}
 
   it { should validate_presence_of :body }
-  
+
   it { should validate_presence_of :question_id}
   
 end
