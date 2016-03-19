0517f716d2c919f9c562b8f6cbbdae4d32931938
Miloserdov Nikita <nmiloserdov09@gmail.com>
2016-03-19 15:15:06 +0300

    Add tests for authentification

--- spec/features/authentication/sign_out_spec.rb
+++ spec/features/authentication/sign_out_spec.rb
@@ -3,6 +3,15 @@ require 'rails_helper'
 feature 'User sign out' do
   given(:user) { create :user }
   
-  scenario 'signed in user try sign out'   
-  scenario 'non-signed in user try sign out'
+  scenario 'signed in user try sign out' do
+    visit root_path
+    sign_in(user)
+    expect(page).to have_content 'log out'
+    click_link 'log out'
+    expect(page).to have_content('Signed out successfully.')
+  end 
+  scenario 'non-signed in user try sign out' do
+    visit root_path
+    expect(page).not_to have_content('log out')
+  end
 end
--- spec/features/authentication/sign_up_spec.rb
+++ spec/features/authentication/sign_up_spec.rb
@@ -1,10 +1,35 @@
 require 'rails_helper'
 
 feature 'sign up user' do
+
+  given(:user) { attributes_for :user }  
   
-  scenario 'sign up user with valid params'
+  before {
+    visit new_user_registration_path
+  }
   
-  scenario 'sign up user with invalid params'
-  # add check for non-repeted  
+  scenario 'sign up user with valid params' do
+    fill_in "Email", with: user[:email]
+    fill_in "Password", with: user[:password]    
+    fill_in "Password confirmation", with: user[:password]    
+    click_on 'Sign up'
+    expect(page).to have_content("Welcome! You have signed up successfully.")
+  end
+  
+  scenario 'sign up with empty params' do
+    fill_in "Email", with: user[:email]    
+    fill_in "Password", with: user[:password]
+    click_on "Sign up"
+    expect(page).to have_content("Password confirmation doesn't match Password")
+  end
+  
+  scenario 'sign up user with existed email' do
+    User.create(user)
+    fill_in "Email", with: user[:email]
+    fill_in "Password", with: user[:password]
+    fill_in "Password confirmation", with: user[:password]
+    click_on 'Sign up'
+    expect(page).to have_content("Email has already been taken")
+  end
   
 end
