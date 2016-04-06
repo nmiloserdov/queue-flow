d61883a2a2b8de93c4ecdc1ca87965dda63ddbbe
Miloserdov Nikita <nmiloserdov09@gmail.com>
2016-04-06 21:55:23 +0300

    Improve feature tests

--- spec/features/question/edit_question_spec.rb
+++ spec/features/question/edit_question_spec.rb
@@ -5,29 +5,22 @@ feature 'user edit question' do
   given(:user) { create(:user) }
   given(:question) { create(:question, user: user) }
 
-  context 'with valid params' do
-
-    scenario "user edit question" , js: true do
-     sign_in user
-     visit question_path(question)
-     click_on "edit"
-
-     within '.edit-question-form' do
-       fill_in "Title", with: "new title"
-       fill_in "Body",  with: "new body"
-       click_on "update"
-     end
-     expect(page).to have_content("new title")
-     expect(page).to have_content("new body")
-    end
-
-  end
-
-  scenario 'with invalid params', js: true do
+  before do
     sign_in user
     visit question_path(question)
     click_on "edit"
+  end
+  scenario "user edit question" , js: true do
+   within '.edit-question-form' do
+     fill_in "Title", with: "new title"
+     fill_in "Body",  with: "new body"
+     click_on "update"
+   end
+   expect(page).to have_content("new title")
+   expect(page).to have_content("new body")
+  end
 
+  scenario 'with invalid params', js: true do
     within '.edit-question-form' do
       fill_in 'Title', with: ""
       fill_in 'Body',  with: ""
