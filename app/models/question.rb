d8c2976038c302b77fcc1913bd248db7612a9be9
Miloserdov Nikita <nmiloserdov09@gmail.com>
2016-04-19 20:37:17 +0300

    Integrate carry with question

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,6 +1,9 @@
 class Question < ActiveRecord::Base
   belongs_to :user 
   has_many   :answers, dependent: :destroy
+  has_many   :attachments 
+
+  accepts_nested_attributes_for :attachments
   
   validates  :title, :body, :user_id, presence: true
 
