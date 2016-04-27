5e697e03234a028176024ddf3473a72fa43fd1ca
Miloserdov Nikita <nmiloserdov09@gmail.com>
2016-04-27 13:07:02 +0300

    Create polimrophic association

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,7 +1,7 @@
 class Question < ActiveRecord::Base
   belongs_to :user 
   has_many   :answers, dependent: :destroy
-  has_many   :attachments 
+  has_many   :attachments, as: :attachmentable, dependent: :destroy
 
   accepts_nested_attributes_for :attachments
   
