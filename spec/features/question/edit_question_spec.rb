eea90d2e0a0db8e5f26d6384af7808d0e093df80
Miloserdov Nikita <nmiloserdov09@gmail.com>
2016-05-12 00:06:30 +0300

    update test for question attachmets

--- spec/features/question/edit_question_spec.rb
+++ spec/features/question/edit_question_spec.rb
@@ -14,10 +14,14 @@ feature 'user edit question' do
    within '.edit-question-form' do
      fill_in "Title", with: "new title"
      fill_in "Body",  with: "new body"
+     click_on "Add file"
+     attach_file "File", "#{Rails.root}/spec/spec_helper.rb"
      click_on "update"
    end
    expect(page).to have_content("new title")
    expect(page).to have_content("new body")
+   expect(page).to have_link 'spec_helper.rb',
+     href: '/uploads/attachment/file/1/spec_helper.rb'
   end
 
   scenario 'with invalid params', js: true do
