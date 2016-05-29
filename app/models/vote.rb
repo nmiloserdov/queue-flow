af1982944b031a0d2c3bd59319dcdba4d0cba15b
Miloserdov Nikita <nmiloserdov09@gmail.com>
2016-05-29 19:55:58 +0300

    FIX bug with votes

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -9,33 +9,42 @@ class Vote < ActiveRecord::Base
 
   enum vote_type: [:down, :up]
   
-  after_create  :process_votable_rang
+  after_create  :process_rang
 
   private
 
-  def process_votable_rang
+  def process_rang
     votable_votes = Vote.where(votable: self.votable, user: user)
-    rang = consider_rang(votable_votes)
-    if votable_votes.count == 2
-      rang = user_rating(votable_votes)
-      self.votable.votes.where(user: user).destroy_all
+    rang = consider_rang + vote_type_to_i
+    if votable_votes.count >= 2
+      rang = user_votes(votable_votes) + consider_rang
     end
     self.votable.update_attribute(:rang, rang)
   end
 
-  def consider_rang(votable_votes)
-    positive = self.votable.votes.where(vote_type: 1).count
-    negative = self.votable.votes.where(vote_type: 0).count
-    positive - negative
-  end
-
-  def user_rating(votable_votes)
+  # logic for non-repeat user votes
+  def user_votes(votable_votes)
     type = votable_votes.map(&:vote_type)
     if type.first == type.second
       vote = Vote.vote_types[type.first] 
+      self.votable.votes.where(user: user).last.destroy
       vote == 0 ? -1 : 1
     else
-      0
+      self.votable.votes.where(user: user).destroy_all
+      return 0
     end
+  end
+
+  # consider rang without user votes
+  def consider_rang
+    votes = self.votable.votes.where.not(user: self.user)
+    positive = votes.where(vote_type: 1).count
+    negative = votes.where(vote_type: 0).count
+    positive - negative
+  end
+
+  def vote_type_to_i
+    vote = Vote.vote_types[self.vote_type]
+    vote == 0 ? -1 : 1
   end
 end
