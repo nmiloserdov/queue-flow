28829e8354144fee9f64b18aa73ca33565023e32
Miloserdov Nikita <nmiloserdov09@gmail.com>
2016-05-31 23:48:15 +0300

    refactoring

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -7,44 +7,22 @@ class Vote < ActiveRecord::Base
   validates :user_id, :vote_type, presence: true
   validates_with VoteValidator
 
-  enum vote_type: [:down, :up]
+  enum vote_type: { down: -1, up: 1 }
   
   after_create  :process_rang
 
   private
 
   def process_rang
-    votable_votes = Vote.where(votable: self.votable, user: user)
-    rang = consider_rang + vote_type_to_i
-    if votable_votes.count >= 2
-      rang = user_votes(votable_votes) + consider_rang
+    votes = Vote.where(votable: self.votable, user: user)
+    if votes.count >= 2
+      if votes.map(&:vote_type).inject { |f,l| f==l  }
+        votes.last.destroy
+      else
+        votes.destroy_all
+      end
     end
+    rang = self.votable.votes.sum(:vote_type)
     self.votable.update_attribute(:rang, rang)
-  end
-
-  # logic for non-repeat user votes
-  def user_votes(votable_votes)
-    type = votable_votes.map(&:vote_type)
-    if type.first == type.second
-      vote = Vote.vote_types[type.first] 
-      self.votable.votes.where(user: user).last.destroy
-      vote == 0 ? -1 : 1
-    else
-      self.votable.votes.where(user: user).destroy_all
-      return 0
-    end
-  end
-
-  # consider rang without user votes
-  def consider_rang
-    votes = self.votable.votes.where.not(user: self.user)
-    positive = votes.where(vote_type: 1).count
-    negative = votes.where(vote_type: 0).count
-    positive - negative
-  end
-
-  def vote_type_to_i
-    vote = Vote.vote_types[self.vote_type]
-    vote == 0 ? -1 : 1
   end
 end
--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -5,7 +5,8 @@ RSpec.describe Vote, type: :model do
   it { should validate_presence_of :user_id }
   it { should validate_presence_of :vote_type }
 
-  let!(:user)     { create(:user) }
+  let!(:user)         { create(:user) }
+
   let!(:question) { create(:question) }
   let!(:answer)   { create(:answer) }
   let(:question_of_the_owner) { create(:question, user: user) }
@@ -19,14 +20,14 @@ RSpec.describe Vote, type: :model do
   let(:down_answer_vote) { create(:down_vote, user: user, votable: answer) }
   let(:owner_as_vote)    { create(:up_vote,   user: user, votable: answer_of_the_owner) }
 
-  describe "validate vote before created" do
+  describe "validate before created" do
     it "not creates vote if user is owner of votable model" do
       expect { owner_qs_vote }.to raise_error(ActiveRecord::RecordInvalid)
       expect { owner_as_vote }.to raise_error(ActiveRecord::RecordInvalid)
     end
 
-    context "not creates vote if user voting twice" do
-      it 'up vote' do
+    context "not creates if user voting twice" do
+      it 'up' do
         2.times {
           create(:up_vote, user: user, votable: question)
           create(:up_vote, user: user, votable: answer)
@@ -35,7 +36,7 @@ RSpec.describe Vote, type: :model do
         expect(answer.rang).to eq(1)
       end
 
-      it 'down vote' do
+      it 'down' do
         2.times {
           create(:down_vote, user: user, votable: question)
           create(:down_vote, user: user, votable: answer)
@@ -44,21 +45,21 @@ RSpec.describe Vote, type: :model do
         expect(answer.rang).to eq(-1)
       end
 
-      it 'ups and deletes vote' do
+      it 'ups and deletes' do
         create(:up_vote,   user: user, votable: question)
         create(:down_vote, user: user, votable: question)
         expect(question.votes.count).to eq 0
         expect(question.rang).to eq 0 
       end
 
-      it 'downs and deletes vote' do
+      it 'downs and deletes' do
         create(:down_vote, user: user, votable: question)
         create(:up_vote,   user: user, votable: question)
         expect(question.votes.count).to eq 0
         expect(question.rang).to eq 0 
       end
 
-      it 'ups and downs vote' do
+      it 'ups and downs' do
         create(:up_vote,   user: user, votable: question)
         create(:down_vote, user: user, votable: question)
         create(:down_vote, user: user, votable: question)
@@ -66,7 +67,7 @@ RSpec.describe Vote, type: :model do
         expect(question.rang).to eq(-1)
       end
 
-      it 'downs and ups vote' do
+      it 'downs and ups' do
         create(:down_vote,   user: user, votable: question)
         create(:up_vote, user: user, votable: question)
         create(:up_vote, user: user, votable: question)
@@ -80,11 +81,11 @@ RSpec.describe Vote, type: :model do
     context "question" do
       before  { question.rang = 0 }
 
-      it "down vote" do
+      it "down" do
         expect{ down_question_vote }.to change{ question.rang }.from(0).to(-1)
       end
 
-      it "up vote" do
+      it "up" do
         expect{ up_question_vote }.to change{ question.rang }.from(0).to(1)
       end
     end
@@ -92,13 +93,28 @@ RSpec.describe Vote, type: :model do
     context "answer" do
       before { answer.rang = 0 }
 
-      it "down vote" do
+      it "down" do
         expect{ down_answer_vote }.to change{ answer.rang }.from(0).to(-1)
       end
 
-      it "up vote" do
+      it "up" do
         expect{ up_answer_vote }.to change{ answer.rang }.from(0).to(1)
       end
+    end
+  end
+
+  context "two users" do
+    let!(:another_user) { create(:user) }
+
+    it "when to ups" do
+      create(:up_vote, user: user, votable: question)
+      create(:up_vote, user: another_user, votable: question)
+      expect(question.rang).to eq 2
+    end
+    it "when ups and downs" do
+      create(:up_vote, user: user, votable: question)
+      create(:down_vote, user: another_user, votable: question)
+      expect(question.rang).to eq 0
     end
   end
 end
