ab37b84d5cb76e065cbf9db23c2c97c13dd432d3
Miloserdov Nikita <nmiloserdov09@gmail.com>
2016-06-02 23:38:16 +0300

    improve routes add concern for vote && comments

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,21 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "questions#index"
   
-  resources :attachments, only: [:destroy]
-  resources :questions do
+  concern :votable do
     patch :vote
-    resources :answers, only: [:create, :destroy, :update,:edit], shallow: true do
-      patch :vote
-      patch :best
+  end
+
+  concern :commentable do
+    resources :comments, only: [:create], shallow: true
+  end
+  resources :comments, only: [:destroy]
+
+  resources :attachments, only: [:destroy]
+
+  resources :questions, concerns: [:commentable, :votable] do
+    resources :answers, concerns: [:commentable, :votable], only:
+      [:create, :destroy, :update, :edit], shallow: true do
+        patch :best
     end
   end
 end
